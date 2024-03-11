4d1888536d95b198abe4fb0b8820214c55092be2
Makomborero Magaya <makomborero.magaya@tsl.co.zw>
2024-03-11 09:06:59 +0200

    implementing the API methods with serializers

---
+++ healthapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Data
+from .serializer import DataSerializer
+
+# Serializing data and return it as a JSON response
+@api_view(['GET'])
+def getData(request):
+    app = Data.objects.all()
+    serializer = DataSerializer(app, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def postData(request):
+    serializer = DataSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
